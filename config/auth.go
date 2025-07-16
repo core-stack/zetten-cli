8805de8830c4c6f8527ec8b503b2c08e48396097
mayron1806 <mayronfernandes01@gmail.com>
2025-07-16 07:34:35 -0300

    chore: start load auth config

--- config/auth.go
+++ config/auth.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/goccy/go-yaml"
 )
@@ -23,6 +25,53 @@ func LoadAuthForHost(host string) (*AuthConfig, error) {
 		filepath.Join(os.Getenv("HOME"), fmt.Sprintf(".zetten/%s", AUTH_FILE_NAME)), // global
 	}
 
+	for _, path := range paths {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			continue
+		}
+
+		var auths AuthMap
+		if err := yaml.Unmarshal(data, &auths); err != nil {
+			return nil, fmt.Errorf("invalid YAML in %s: %w", path, err)
+		}
+		for authHost := range auths {
+			if strings.HasPrefix(authHost, host) {
+				
+			}
+		}
+		if config, ok := auths[host]; ok {
+			return &config, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no auth config found for host: %s", host)
+}
+
+type AuthConfigLoader
+
+func loadByHostPath(host string, path string) (*AuthConfig, error) {
+
+}
+func LoadAuthForUrl(repoUrl string) (*AuthConfig, error) {
+	paths := []string{
+		AUTH_FILE_NAME, // local
+		filepath.Join(os.Getenv("HOME"), fmt.Sprintf(".zetten/%s", AUTH_FILE_NAME)), // global
+	}
+
+	u, err := url.Parse(repoUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse repo URL: %w", err)
+	}
+	
+	host := u.Host
+	urlPaths := strings.Split(u.Path, "/")
+
+	// verify decrental repository path
+	for i := len(urlPaths); i > 0; i-- {
+		
+	}
+	
 	for _, path := range paths {
 		data, err := os.ReadFile(path)
 		if err != nil {
